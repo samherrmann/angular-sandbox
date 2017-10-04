a83a20e24985c93ef8693a0bd14cce00bfc1b059
Sam Herrmann <samherrmann.github@gmail.com>
2017-10-03 22:49:54 -0400

    Explore ViewChildren query with createEmbeddedView

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, ViewChild, TemplateRef, ViewChildren,
+  ChangeDetectorRef, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('vc', { read: ViewContainerRef }) viewContainerRef: ViewContainerRef;
+
+  @ViewChild('tpl') tplRef: TemplateRef<any>;
+
+  @ViewChildren('dynChild') dynChild: QueryList<ElementRef>;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    this.dynChild.changes.subscribe((x) => {
+      console.log(this.dynChild.last);
+    });
+
+    setTimeout(() => {
+      // without calling detect changes after each time an embedded
+      // view is created, only one event is detected.
+
+      for (let i = 0; i < 3; i++) {
+        this.viewContainerRef.createEmbeddedView(this.tplRef);
+        this.changeDetectorRef.detectChanges();
+      }
+    }, 2000);
+  }
 }
