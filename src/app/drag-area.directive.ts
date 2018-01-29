93071f678b0c048160381a98def88e2529f8a7a9
Sam Herrmann <samherrmann.github@gmail.com>
2018-01-29 13:45:48 -0500

    WIP

--- src/app/drag-area.directive.ts
+++ src/app/drag-area.directive.ts
@@ -1,7 +1,10 @@
-import { Directive, HostListener, OnInit, OnDestroy, ElementRef} from '@angular/core';
+import { Directive, HostListener, OnInit, OnDestroy, ElementRef, Renderer2} from '@angular/core';
 import { DragAndDropService } from './drag-and-drop.service';
 import { Subscription } from 'rxjs/Subscription';
 import { filter } from 'rxjs/operators';
+import { DraggableComponent } from './draggable/draggable.component';
+import { Coordinate2D } from './draggable/coordinate-2d';
+import { DragEvent } from './drag-event';
 
 @Directive({
   selector: '[appDraggableArea]'
@@ -10,21 +13,24 @@ export class DragAreaDirective implements OnInit, OnDestroy {
 
   private isActive = false;
 
+  private ghost: HTMLElement;
+
+  private dragStartPoint: Coordinate2D;
+
   private subscriptions: Subscription[] = [];
 
   constructor(private dragAndDropService: DragAndDropService,
-    private elementRef: ElementRef) { }
+    private elementRef: ElementRef,
+    private renderer: Renderer2) { }
 
   ngOnInit() {
     this.subscriptions.push(
       this.dragAndDropService.isActive.subscribe(isActive => {
         this.isActive = isActive;
       }),
-      this.dragAndDropService.events.pipe(
-        filter(e => e.type === 'dragstart')
-      ).subscribe(e => {
-        this.createGhostImage(e.draggable.componetRef.location.nativeElement);
-      })
+      this.handleDragStarEvents(),
+      this.handleDragEvents(),
+      this.handledragEndEvents()
     );
   }
 
@@ -33,6 +39,8 @@ export class DragAreaDirective implements OnInit, OnDestroy {
     if (this.isActive) {
       this.dragAndDropService.drag(e);
     }
+    e.stopPropagation();
+    return false;
   }
 
   @HostListener('pointerup', ['$event'])
@@ -44,10 +52,59 @@ export class DragAreaDirective implements OnInit, OnDestroy {
     this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
-  private createGhostImage(el: HTMLElement) {
+  private handleDragStarEvents(): Subscription {
+    return this.dragAndDropService.events.pipe(
+      filter(e => e.type === 'dragstart')
+    ).subscribe(e => {
+      this.renderer.setStyle(this.elementRef.nativeElement, 'user-select', 'none');
+
+      this.dragStartPoint = {
+        x: e.pointerEvent.clientX,
+        y: e.pointerEvent.clientY
+      };
+      this.createGhostImage(e.draggable);
+    });
+  }
+
+  private handleDragEvents(): Subscription {
+    return this.dragAndDropService.events.pipe(
+      filter(e => e.type === 'drag')
+    ).subscribe(e => {
+      this.moveGhost(e.pointerEvent);
+    });
+  }
+
+  private handledragEndEvents(): Subscription {
+    return this.dragAndDropService.events.pipe(
+      filter(e => e.type === 'dragend')
+    ).subscribe(e => {
+      this.removeGhost();
+      this.renderer.removeStyle(this.elementRef.nativeElement, 'user-select');
+    });
+  }
+
+  private createGhostImage(draggable: DraggableComponent): void {
+    const container = <HTMLElement>this.elementRef.nativeElement;
+    const el = <HTMLElement>draggable.componetRef.location.nativeElement;
     const clone = <HTMLElement>el.cloneNode(true);
+    const clientRect = el.getBoundingClientRect();
+
     clone.style.width = el.offsetWidth + 'px';
     clone.style.height = el.offsetHeight + 'px';
-    this.elementRef.nativeElement.appendChild(clone);
+    clone.style.position = 'fixed';
+    clone.style.top = clientRect.top + 'px';
+    clone.style.left = clientRect.left + 'px';
+    this.ghost = container.appendChild(clone);
+  }
+
+  private moveGhost(e: PointerEvent): void {
+    const deltaX = e.clientX - this.dragStartPoint.x;
+    const deltaY = e.clientY - this.dragStartPoint.y;
+    this.ghost.style.transform = 'translate3d(' + deltaX + 'px, ' + deltaY + 'px, 0)';
+  }
+
+  private removeGhost(): void {
+    this.elementRef.nativeElement.removeChild(this.ghost);
+    this.ghost = undefined;
   }
 }
