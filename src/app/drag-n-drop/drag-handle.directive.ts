d0424962a07813f518ac36eed3d7b835b7e05a69
Sam Herrmann <samherrmann.github@gmail.com>
2018-01-31 18:13:53 -0500

    Move touch-action style to drag handle

--- src/app/drag-n-drop/drag-handle.directive.ts
+++ src/app/drag-n-drop/drag-handle.directive.ts
@@ -1,15 +1,20 @@
-import { Directive, HostListener, SkipSelf } from '@angular/core';
+import { Directive, HostListener, SkipSelf, Renderer2, ElementRef, OnInit } from '@angular/core';
 import { DragNDropService } from './drag-n-drop.service';
 import { DraggableComponent } from './draggable/draggable.component';
 
 @Directive({
   selector: '[appDragHandle]'
 })
-export class DragHandleDirective {
+export class DragHandleDirective implements OnInit {
 
   constructor(private dragAndDropService: DragNDropService,
-    @SkipSelf() private draggable: DraggableComponent) { }
+    @SkipSelf() private draggable: DraggableComponent,
+    private renderer: Renderer2,
+    private elementRef: ElementRef) { }
 
+  ngOnInit() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'touch-action', 'none');
+  }
 
   @HostListener('pointerdown', ['$event'])
   pointerDown(e: PointerEvent) {
