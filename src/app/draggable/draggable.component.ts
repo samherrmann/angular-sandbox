a32887e893c6b932967ac6f7ba5de97619e71524
Sam Herrmann <samherrmann.github@gmail.com>
2018-01-31 12:45:36 -0500

    Handle draggable tansit styles in component

--- src/app/draggable/draggable.component.ts
+++ src/app/draggable/draggable.component.ts
@@ -1,5 +1,5 @@
 import { Component, HostListener, OnInit, OnDestroy,
-  ComponentRef, ViewChild, ViewContainerRef, HostBinding } from '@angular/core';
+  ComponentRef, ViewChild, ViewContainerRef, HostBinding, ElementRef } from '@angular/core';
 import {  DraggableService } from './draggable.service';
 import { DragAndDropService } from '../drag-and-drop.service';
 import { DroppableComponent } from '../droppable/droppable.component';
@@ -18,8 +18,23 @@ export class DraggableComponent implements OnInit, OnDestroy {
   @ViewChild('vc', { read: ViewContainerRef })
   viewContainerRef: ViewContainerRef;
 
+  @HostBinding('class.in-transit')
+  isInTransit = false;
+
   @HostBinding('style.transform')
   transform = 'none';
+
+  @HostBinding('style.width')
+  width: string;
+
+  @HostBinding('style.height')
+  height: string;
+
+  @HostBinding('style.top')
+  top: string;
+
+  @HostBinding('style.left')
+  left: string;
 
   componetRef: ComponentRef<DraggableComponent>;
 
@@ -27,13 +42,15 @@ export class DraggableComponent implements OnInit, OnDestroy {
 
   private subscriptions: Subscription[] = [];
 
-  constructor(private dragAndDropService: DragAndDropService,
+  constructor(private elementRef: ElementRef,
+    private dragAndDropService: DragAndDropService,
     private draggableService: DraggableService) { }
 
   ngOnInit() {
-    this.draggableService.register(this);
     this.subscriptions.push(
-      this.handleDrag()
+      this.handleDragStart(),
+      this.handleDrag(),
+      this.handleDragEnd()
     );
   }
 
@@ -46,9 +63,33 @@ export class DraggableComponent implements OnInit, OnDestroy {
     this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 
+  private handleDragStart() {
+    return this.draggableService.dragStartEvents(this).subscribe(e => {
+      const el: HTMLElement = this.elementRef.nativeElement;
+      const clientRect = el.getBoundingClientRect();
+
+      this.isInTransit = true;
+      this.width = el.offsetWidth + 'px';
+      this.height = el.offsetHeight + 'px';
+      this.top = clientRect.top + 'px';
+      this.left = clientRect.left + 'px';
+    });
+  }
+
   private handleDrag() {
     return this.draggableService.dragEvents(this).subscribe(delta => {
       this.transform = 'translate(' + delta.x + 'px, ' + delta.y + 'px)';
+    });
+  }
+
+  private handleDragEnd() {
+    return this.draggableService.dragEndEvents(this).subscribe(delta => {
+      this.isInTransit = false;
+      this.transform = null;
+      this.width = null;
+      this.height = null;
+      this.top = null;
+      this.left = null;
     });
   }
 }
--- src/app/draggable/draggable.service.ts
+++ src/app/draggable/draggable.service.ts
@@ -1,28 +1,26 @@
-import { Injectable, OnDestroy, RendererFactory2 } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { DragAndDropService } from '../drag-and-drop.service';
 import { Observable } from 'rxjs/Observable';
 import { DragEvent, DragEventType } from '../drag-event';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 import { DraggableComponent } from './draggable.component';
-import { Subscription } from 'rxjs/Subscription';
 import { Coordinate2D } from '../coordinate-2d';
 
 @Injectable()
-export class DraggableService implements OnDestroy {
+export class DraggableService {
 
   private dragStartPoint: Coordinate2D;
 
-  private subscriptions: Subscription[] = [];
+  constructor(private dragAndDropService: DragAndDropService) { }
 
-  private readonly renderer = this.rendererFactory.createRenderer(null, null);
-
-  constructor(private dragAndDropService: DragAndDropService,
-    private rendererFactory: RendererFactory2) { }
-
-  register(draggable: DraggableComponent): void {
-    this.subscriptions.push(
-      this.handleDragStarEvents(draggable),
-      this.handleDragEndEvents(draggable)
+  dragStartEvents(draggable: DraggableComponent): Observable<DragEvent> {
+    return this.events(draggable, 'dragstart').pipe(
+      tap(e => {
+        this.dragStartPoint = {
+          x: e.pointerEvent.clientX,
+          y: e.pointerEvent.clientY
+        };
+      })
     );
   }
 
@@ -38,42 +36,17 @@ export class DraggableService implements OnDestroy {
     );
   }
 
-  private handleDragStarEvents(draggable: DraggableComponent): Subscription {
-    return this.events(draggable, 'dragstart').subscribe(e => {
-      this.dragStartPoint = {
-        x: e.pointerEvent.clientX,
-        y: e.pointerEvent.clientY
-      };
-      this.setTransitStyles(draggable.componetRef.location.nativeElement);
-    });
-  }
-
-  private handleDragEndEvents(draggable: DraggableComponent): Subscription {
-    return this.events(draggable, 'dragend').subscribe(e => {
-      this.removeTransientStyles(e.draggable.componetRef.location.nativeElement);
-    });
+  dragEndEvents(draggable: DraggableComponent): Observable<DragEvent> {
+    return this.events(draggable, 'dragend').pipe(
+      tap(e => {
+        this.dragStartPoint = null;
+      })
+    );
   }
 
   private events(draggable: DraggableComponent, type?: DragEventType): Observable<DragEvent> {
     return this.dragAndDropService.events(type).pipe(
       filter(e => e.draggable === draggable)
     );
-  }
-
-  private setTransitStyles(draggable: HTMLElement): void {
-    const clientRect = draggable.getBoundingClientRect();
-    this.renderer.setStyle(draggable, 'width', draggable.offsetWidth + 'px');
-    this.renderer.setStyle(draggable, 'height', draggable.offsetHeight + 'px');
-    this.renderer.setStyle(draggable, 'position', 'fixed');
-    this.renderer.setStyle(draggable, 'top', clientRect.top + 'px');
-    this.renderer.setStyle(draggable, 'left', clientRect.left + 'px');
-  }
-
-  private removeTransientStyles(draggable: HTMLElement): void {
-    this.renderer.removeAttribute(draggable, 'style');
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach(sub => sub.unsubscribe());
   }
 }
