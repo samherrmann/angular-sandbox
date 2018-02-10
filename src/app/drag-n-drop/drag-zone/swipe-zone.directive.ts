707fd9978cb05c17116b49802b4435b22e7537fe
Sam Herrmann <samherrmann.github@gmail.com>
2018-02-10 16:14:29 -0500

    Update comments on automatically adding  directives

--- src/app/drag-n-drop/drag-zone/swipe-zone.directive.ts
+++ src/app/drag-n-drop/drag-zone/swipe-zone.directive.ts
@@ -2,12 +2,11 @@ import { Directive } from '@angular/core';
 import { SwipeZoneService } from '../../swipe/swipe-zone.service';
 import { SwipeZoneDirective as BaseDirective } from '../../swipe/swipe-zone.directive';
 
-// This is a temporary solution of adding a directive to
-// the host element of another directive. As of Angular v5,
-// it is not possible to achieve that in a clean, concise
-// way. It is expected that this ability will become
-// available after Angular v6.
-// See https://github.com/angular/angular/issues/8785
+/**
+ * This definition enables adding the {@link SwipeZoneDirective}
+ * on the same host element as the {@link DragZoneDirective}.
+ * See https://github.com/angular/angular/issues/8785
+ */
 @Directive({
   selector: '[appDragZone]',
   providers: [
--- src/app/drag-n-drop/droppable/swipe-target.directive.ts
+++ src/app/drag-n-drop/droppable/swipe-target.directive.ts
@@ -2,18 +2,17 @@ import { Directive } from '@angular/core';
 import { SwipeTargetService } from '../../swipe/swipe-target.service';
 import { SwipeTargetDirective as BaseDirective } from '../../swipe/swipe-target.directive';
 
-// This is a temporary solution of adding a directive to
-// the component host element. As of Angular v5, it is not
-// possible to achieve that in a clean, concise way. It is
-// expected that this ability will become available after
-// Angular v6.
-// See https://github.com/angular/angular/issues/8785
-//
-// We need to add a tslint exception here because tslint
-// enforces that directives use attribute selectors and
-// not element selectors. In this case we need the selector
-// to match the selector of the component on which we want
-// to apply this directive.
+/**
+ * This definition enables adding the {@link SwipeTargetDirective}
+ * on the same host element as the {@link DroppableComponent}.
+ * See https://github.com/angular/angular/issues/8785
+ *
+ * We need to add a tslint exception here because tslint
+ * enforces that directives use attribute selectors and
+ * not element selectors. In this case we need the selector
+ * to match the selector of the component on which we want
+ * to apply this directive.
+ */
 // tslint:disable:directive-selector
 @Directive({
   selector: 'app-droppable',
