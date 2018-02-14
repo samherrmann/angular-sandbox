359e49d37babd2b8edbb5e5c8f95ef96143d89fb
Sam Herrmann <samherrmann.github@gmail.com>
2018-02-14 15:12:42 -0500

    Refactor relocation service

--- src/app/drag-and-drop/relocation.service.ts
+++ src/app/drag-and-drop/relocation.service.ts
@@ -7,42 +7,40 @@ import { RelocationEvent } from './relocation-event';
 @Injectable()
 export class RelocationService implements OnDestroy {
 
+  private draggableRelocation = this.dragAndDropService.dragEnter.pipe(
+    map(e => {
+      const target = e.dropZone.location();
+      let index = target.index;
+
+      if (index) {
+        // adjust the index if the draggable is currently located before the drop-zone
+        // in the same container, i.e. if that's the case, the index of the drop-zone
+        // will be reduced by one when the draggable is removed from its current location.
+        if (target.droppable === e.draggable.droppable && e.draggable.index() < index) {
+          index -= 1;
+        }
+      }
+      return new RelocationEvent(e.pointerEvent, e.draggable, target.droppable, index);
+    }),
+    filter(e => {
+      // only emit a relocation event if the requested position is different from the
+      // current position.
+      return !(e.draggable.droppable === e.droppable && e.draggable.index() === e.index);
+    })
+  );
+
   private subs: Subscription[] = [];
 
   constructor(private dragAndDropService: DragAndDropService) { }
 
   init() {
     this.subs.push(
-      this.handleDragEnter()
-    );
-  }
-
-  private handleDragEnter(): Subscription {
-    return this.dragAndDropService.dragEnter.pipe(
-      map(e => {
-        const target = e.dropZone.location();
-        let index = target.index;
-
-        if (index) {
-          // adjust the index if the draggable is currently located before the drop-zone
-          // in the same container, i.e. if that's the case, the index of the drop-zone
-          // will be reduced by one when the draggable is removed from its current location.
-          if (target.droppable === e.draggable.droppable && e.draggable.index() < index) {
-            index -= 1;
-          }
-        }
-        return new RelocationEvent(e.pointerEvent, e.draggable, target.droppable, index);
-      }),
-      filter(e => {
-        return !(e.draggable.droppable === e.droppable && e.draggable.index() === e.index);
+      this.draggableRelocation.subscribe(e => {
+        e.draggable.detatch();
+        e.draggable.insert(e.droppable, e.index);
+        this.dragAndDropService.emitDrop(e.pointerEvent, e.draggable);
       })
-    ).subscribe(e => this.moveDraggable(e));
-  }
-
-  private moveDraggable(e: RelocationEvent): void {
-    e.draggable.detatch();
-    e.draggable.insert(e.droppable, e.index);
-    this.dragAndDropService.emitDrop(e.pointerEvent, e.draggable);
+    );
   }
 
   ngOnDestroy() {
