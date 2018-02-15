06691a4c51f70c3e19a684eeaaca5d059c8d0e85
Sam Herrmann <samherrmann.github@gmail.com>
2018-02-15 14:40:54 -0500

    Add createRelocationEvent method to target-relocation service

--- src/app/drag-and-drop/relocation/target-relocation.service.ts
+++ src/app/drag-and-drop/relocation/target-relocation.service.ts
@@ -10,28 +10,31 @@ import { DragEnterEvent } from '../drag-event';
 export class TargetRelocationService {
 
   readonly relocation = this.dragAndDropService.dragEnter.pipe(
-    map(e => {
-      let relocation: RelocationEvent = null;
-      // if the target draggable is inside a swappable, then we need to move that
-      // draggable into the origin location of the transient draggable.
-      if (this.isOverDraggableTargetInsideSwappable(e) &&
-        !this.cacheRelocationService.isOverCache(e)) {
-        const transientOrigin = this.transientRelocationService.origin();
-
-        relocation = new RelocationEvent(
-          e.pointerEvent,
-          e.dropZone.draggable(),
-          transientOrigin.droppable,
-          transientOrigin.index
-        );
-      }
-      return relocation;
-    })
+    map(e => this.createRelocationEvent(e))
   );
 
   constructor(private dragAndDropService: DragAndDropService,
     private transientRelocationService: TransientRelocationService,
     private cacheRelocationService: CacheRelocationService) { }
+
+  private createRelocationEvent(e: DragEnterEvent): RelocationEvent {
+    let relocation: RelocationEvent = null;
+
+    // if the target draggable is inside a swappable, then we need to move that
+    // draggable into the origin location of the transient draggable.
+    if (this.isOverDraggableTargetInsideSwappable(e) &&
+      !this.cacheRelocationService.isOverCache(e)) {
+      const transientOrigin = this.transientRelocationService.origin();
+
+      relocation = new RelocationEvent(
+        e.pointerEvent,
+        e.dropZone.draggable(),
+        transientOrigin.droppable,
+        transientOrigin.index
+      );
+    }
+    return relocation;
+  }
 
   private isOverDraggableTargetInsideSwappable(e: DragEnterEvent): boolean {
     return e.dropZone.draggable() !== null && e.dropZone.location().droppable.swappable;
