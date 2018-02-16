853e2998bce141dd248cbc83f0d95a27b7abda91
Sam Herrmann <samherrmann.github@gmail.com>
2018-02-16 12:01:42 -0500

    Remove home button when draggable is at origin

--- src/app/drag-and-drop/draggable-home-button.directive.ts
+++ src/app/drag-and-drop/draggable-home-button.directive.ts
@@ -1,22 +1,64 @@
-import { Directive, SkipSelf, HostListener } from '@angular/core';
+import { Directive, OnInit, OnDestroy, SkipSelf, TemplateRef, ViewContainerRef, EmbeddedViewRef, Renderer2 } from '@angular/core';
 import { DraggableComponent } from './draggable/draggable.component';
+import { DraggableService } from './draggable/draggable.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Directive({
   selector: '[appDraggableHomeButton]'
 })
-export class DraggableHomeButtonDirective {
+export class DraggableHomeButtonDirective implements OnInit, OnDestroy {
 
-  constructor(@SkipSelf() private draggable: DraggableComponent) {}
+  private viewRef: EmbeddedViewRef<DraggableHomeButtonDirective> = null;
 
-  @HostListener('click')
-  click() {
-    if (this.draggable.droppable !== this.draggable.origin()) {
-      this.draggable.detatch();
-      if (this.draggable.origin()) {
-        this.draggable.insert(this.draggable.origin());
-      } else {
-        this.draggable.remove();
+  private removeClickListener: () => void;
+
+  private sub: Subscription;
+
+  constructor(@SkipSelf() private draggable: DraggableComponent,
+    private draggableService: DraggableService,
+    private templateRef: TemplateRef<DraggableHomeButtonDirective>,
+    private viewContainerRef: ViewContainerRef,
+    private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.sub = this.draggableService.insert.subscribe(e => {
+      if (!this.doesViewExist() && !this.isAtOrigin()) {
+        this.createView();
+
+      } else if (this.doesViewExist() && this.isAtOrigin()) {
+        this.removeView();
       }
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  private createView(): void {
+    this.viewRef = this.viewContainerRef.createEmbeddedView(this.templateRef);
+    this.removeClickListener = this.renderer.listen(this.viewRef.rootNodes[0], 'click', () => this.onClick());
+  }
+
+  private removeView(): void {
+    this.viewContainerRef.remove(this.viewContainerRef.indexOf(this.viewRef));
+    this.removeClickListener();
+  }
+
+  private doesViewExist(): boolean {
+    return this.viewRef !== null && !this.viewRef.destroyed;
+  }
+
+  private isAtOrigin(): boolean {
+    return this.draggable.droppable === this.draggable.origin();
+  }
+
+  private onClick(): void {
+    this.draggable.detatch();
+    if (this.draggable.origin()) {
+      this.draggable.insert(this.draggable.origin());
+    } else {
+      this.draggable.remove();
     }
   }
 }
