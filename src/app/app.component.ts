23bb08809acfd8577fdc3bfa4e12a69b063f3c78
Sam Herrmann <samherrmann.github@gmail.com>
2018-02-22 12:05:41 -0500

    Create draggables in template directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { Component, ViewChildren, QueryList, ViewChild, ChangeDetectorRef } from '@angular/core';
-import { DroppableComponent } from './drag-and-drop/droppable/droppable.component';
-import { TemplateDirective } from './drag-and-drop/template.directive';
+import { Component } from '@angular/core';
 import { slideInOut } from './animations';
-import { DataService, Media } from './data.service';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -16,23 +14,11 @@ export class AppComponent {
 
   isDrawerOpen = false;
 
-  @ViewChildren(DroppableComponent)
-  droppables: QueryList<DroppableComponent>;
+  videos = this.dataService.videos;
 
-  @ViewChild(TemplateDirective)
-  draggableTpl: TemplateDirective<Media>;
-
-  constructor(private dataService: DataService,
-    private cdr: ChangeDetectorRef) { }
+  constructor(private dataService: DataService) { }
 
   toggleDrawer(): void {
     this.isDrawerOpen = !this.isDrawerOpen;
-    if (this.isDrawerOpen) {
-      this.cdr.detectChanges();
-      const drawer = this.droppables.find(item => item.name === 'drawer');
-      this.dataService.videos.forEach((video, i) => {
-        this.draggableTpl.create('example-' + i, drawer, video);
-      });
-    }
   }
 }
--- src/app/drag-and-drop/template.directive.ts
+++ src/app/drag-and-drop/template.directive.ts
@@ -1,17 +1,41 @@
-import { Directive, TemplateRef } from '@angular/core';
+import { Directive, OnInit, TemplateRef, Input, SkipSelf } from '@angular/core';
 import { DraggableFactoryService } from './draggable/draggable-factory.service';
 import { DroppableComponent } from './droppable/droppable.component';
 
 @Directive({
   selector: '[dndTemplate]'
 })
-export class TemplateDirective<T> {
+export class TemplateDirective implements OnInit {
+
+  @Input()
+  dndTemplateOf: any[];
+
+  @Input()
+  dndTemplateGroup = '';
 
   constructor(private templateRef: TemplateRef<any>,
-    private draggableFactoryService: DraggableFactoryService, ) {
+    private draggableFactoryService: DraggableFactoryService,
+    @SkipSelf() private droppable: DroppableComponent) {
   }
 
-  create(id: string, droppable: DroppableComponent, ctx?: T): void {
-    this.draggableFactoryService.create(id, this.templateRef, droppable, ctx);
+  ngOnInit() {
+    if (this.dndTemplateOf instanceof Array) {
+      this.dndTemplateOf.forEach((item, i) => {
+        const ctx = {
+          $implicit: item,
+          index: i
+        };
+
+        this.draggableFactoryService.create<any>(
+          this.dndTemplateGroup + '-' + i,
+          this.templateRef,
+          this.droppable,
+          ctx
+        );
+      });
+
+    } else {
+      console.error('dndTemplateOf is not of type Array');
+    }
   }
 }
