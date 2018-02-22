72d978383bc2d58d27212769d76964dbe4c2aded
Sam Herrmann <samherrmann.github@gmail.com>
2018-02-22 08:39:57 -0500

    Move video outside of example component

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-example',
@@ -7,12 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ExampleComponent implements OnInit {
 
-  @Input()
-  src = '';
-
   constructor() { }
 
   ngOnInit() {
   }
-
 }
