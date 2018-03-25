6564519b2b2df2c816c90c35192b9893d7585ee5
Sam Herrmann <samherrmann.github@gmail.com>
2018-03-25 17:10:49 -0400

    Add different sample videos

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,9 +6,9 @@ export class DataService {
   readonly videos: Media[] = [{
     src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
   }, {
-    src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'
   }, {
-    src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+    src: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4'
   }];
 }
 
