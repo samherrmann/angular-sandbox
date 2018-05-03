dc01605d66f566a92fc002aaf63835b2d8b349d5
Sam Herrmann <samherrmann.github@gmail.com>
2018-05-03 13:25:39 -0400

    Add timer Observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  output = timer(0, 1000);
 }
