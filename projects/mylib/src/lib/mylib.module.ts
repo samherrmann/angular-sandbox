0155b17e5873f52d41c4cd6a3921035506060cc5
Sam Herrmann <samherrmann.github@gmail.com>
2018-08-22 15:45:28 -0400

    Set up scenario

--- projects/mylib/src/lib/mylib.module.ts
+++ projects/mylib/src/lib/mylib.module.ts
@@ -1,10 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { MylibComponent } from './mylib.component';
+
+export function myLibInit() {
+  return () => console.log('Hi from exported function');
+}
 
 @NgModule({
   imports: [
   ],
   declarations: [MylibComponent],
-  exports: [MylibComponent]
+  exports: [MylibComponent],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      multi: true,
+      /**
+       * Option 1. Defining the useFactory function as an exported function as instructed in
+       * the docs - https://angular.io/guide/aot-compiler#function-calls-are-not-supported:
+       * - Throws build error in both dev and prod: "Lambda not supported"
+       */
+      useFactory: myLibInit
+
+      /**
+       * Option 2. Defining the useFactory function using the ES6 object method shorthand notation:
+       * - Builds successfully in both dev and prod
+       * - No runtime error in dev
+       * - Throws runtime error in prod: "TypeError: this.appInits[i] is not a function"
+       */
+      // useFactory() {
+      //   return () => console.log('Hi from ES6 object method shorthand')
+      // }
+    }
+  ]
 })
 export class MylibModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MylibModule } from 'mylib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MylibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
