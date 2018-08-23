c179374382c14e05758b1bf0d3ca442cc01a2ef5
Sam Herrmann <samherrmann.github@gmail.com>
2018-08-22 21:46:38 -0400

    Fix "Lambda not supported" build error

--- projects/mylib/src/lib/mylib.module.ts
+++ projects/mylib/src/lib/mylib.module.ts
@@ -2,7 +2,8 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { MylibComponent } from './mylib.component';
 
 export function myLibInit() {
-  return () => console.log('Hi from exported function');
+  const fn = () => { console.log('Hi from exported function') };
+  return fn;
 }
 
 @NgModule({
@@ -14,22 +15,7 @@ export function myLibInit() {
     {
       provide: APP_INITIALIZER,
       multi: true,
-      /**
-       * Option 1. Defining the useFactory function as an exported function as instructed in
-       * the docs - https://angular.io/guide/aot-compiler#function-calls-are-not-supported:
-       * - Throws build error in both dev and prod: "Lambda not supported"
-       */
       useFactory: myLibInit
-
-      /**
-       * Option 2. Defining the useFactory function using the ES6 object method shorthand notation:
-       * - Builds successfully in both dev and prod
-       * - No runtime error in dev
-       * - Throws runtime error in prod: "TypeError: this.appInits[i] is not a function"
-       */
-      // useFactory() {
-      //   return () => console.log('Hi from ES6 object method shorthand')
-      // }
     }
   ]
 })
