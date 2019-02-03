3aa7447a1fce80931d9b16042fbe63882f080539
Sam Herrmann <samherrmann.github@gmail.com>
2019-02-02 21:24:34 -0500

    Import lib in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooModule } from 'foo';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FooModule
   ],
   providers: [],
   bootstrap: [AppComponent]
