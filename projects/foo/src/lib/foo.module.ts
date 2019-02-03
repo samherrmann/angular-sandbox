97b3ac78dc57b30342ce473b2dac171073bf6cb1
Sam Herrmann <samherrmann.github@gmail.com>
2019-02-02 21:24:25 -0500

    Add global type definition to lib

--- projects/foo/src/lib/foo.module.ts
+++ projects/foo/src/lib/foo.module.ts
@@ -1,5 +1,9 @@
+/// <reference path="../typings.d.ts"/>
+
 import { NgModule } from '@angular/core';
 import { FooComponent } from './foo.component';
+
+let x = MyLibrary;
 
 @NgModule({
   declarations: [FooComponent],
---
+++ projects/foo/src/typings.d.ts
@@ -0,0 +1,1 @@
+declare var MyLibrary: any;
