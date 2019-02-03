a0146b8017d596495de4352487d4499ce9075e95
Sam Herrmann <samherrmann.github@gmail.com>
2019-02-02 22:00:25 -0500

    Add libraru foo

---
+++ projects/foo/src/lib/foo.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { FooComponent } from './foo.component';
+
+@NgModule({
+  declarations: [FooComponent],
+  imports: [
+  ],
+  exports: [FooComponent]
+})
+export class FooModule { }
---
+++ projects/foo/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of foo
+ */
+
+export * from './lib/foo.service';
+export * from './lib/foo.component';
+export * from './lib/foo.module';
