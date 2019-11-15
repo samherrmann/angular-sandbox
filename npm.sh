7f47e02ca1b4d1c0df29a318c3642d71ee9fb7ab
Sam Herrmann <samherrmann.github@gmail.com>
2019-11-15 15:21:00 -0500

    Inherit proxy settings from host system

--- npm.sh
+++ npm.sh
@@ -1,12 +1,14 @@
-# This is a sample script for using the container to run npm commands.
-
 args=()
 [[ $@ == 'start'* || $@ == 'run start'* ]] && args+=('--publish 4200:4200')
 [[ $@ == 'test'* || $@ == 'run test'* ]] && args+=('--publish 9876:9876')
+[[ $@ == 'run e2e'* ]] && args+=('--publish 49152:49152')
 
 docker run \
   -it \
   --rm \
   -v $(pwd):/code \
+  -e HTTP_PROXY=$HTTP_PROXY \
+  -e HTTPS_PROXY=$HTTPS_PROXY \
+  -e NO_PROXY=$NO_PROXY \
   ${args} \
   samherrmann/angular-cli:7.3.0 $@
