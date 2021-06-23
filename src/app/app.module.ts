1cde827f8bc22979192a98bd95434dfe21c0e024
Sam Herrmann <samherrmann.github@gmail.com>
2021-06-22 22:03:24 -0400

    Add Scully with two routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
+import { RouterModule, Routes } from '@angular/router';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { OneModule } from './one/one.module';
+import { TwoModule } from './two/two.module';
+
+const routes: Routes = [{
+  path: 'one',
+  component: OneComponent
+}, {
+  path: 'two',
+  component: TwoComponent
+}];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ScullyLibModule,
+    RouterModule.forRoot(routes),
+    OneModule,
+    TwoModule
+  ],
+  declarations: [
+    AppComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
